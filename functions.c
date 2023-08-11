e8ed8edb6cb1d52111aa32d622f97a3fdd9e655a
Gabriel Schuina <gabrielschuina86@gmail.com>
2023-08-10 23:46:48 -0400

    alphab order update

--- functions.c
+++ functions.c
@@ -45,7 +45,7 @@ Node* insertNode(Node* p, char* name, Type type){
         return createNode(name,type);
     } else if(p->child){
         aux = p->child;
-        if(aux->name[0] < name[0]){
+        if(aux->name[0] > name[0]){
             temp = aux;
             p->child = createNode(name,type);
             p->child->next = temp;
